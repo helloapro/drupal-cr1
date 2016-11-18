2c2dc07b62bbc529372a52dca28160b98ef13bf8
April Peng <helloaprilpeng@gmail.com>
2016-11-18 08:58:31 -0800

    install ctools, features, views, and strongarm modules

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -212,7 +212,21 @@
  *   );
  * @endcode
  */
-$databases = array();
+$databases = array (
+  'default' => 
+  array (
+    'default' => 
+    array (
+      'database' => 'bookstore',
+      'username' => 'bookstore',
+      'password' => 'bookstore',
+      'host' => '127.0.0.1',
+      'port' => '8889',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
 
 /**
  * Access control for update.php script.
@@ -244,7 +258,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = '';
+$drupal_hash_salt = 'qUdc3z0MHzp1ibuxS5U24RlWoW31vSS8an4dHM1J8hw';
 
 /**
  * Base URL (optional).
